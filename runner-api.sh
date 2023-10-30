dc621fece5e9b962bfbe608bc7bc49d3ce40a867
Automation test manager <ashkumark@gmail.com>
2023-10-30 22:29:55 +0000

    Updated jenkinsfile 2

--- runner-api.sh
+++ runner-api.sh
@@ -2,6 +2,7 @@
 
 echo "Run automated API tests (using runner script)..."
 #mvn -f pom.xml test -Dtest=TestRunner -Dcucumber.filter.tags=$TYPE
-#mvn test -Dtest=TestRunner -Dcucumber.filter.tags=$TYPE
-mvn test -Dcucumber.filter.tags=$TYPE
+mvn test -Dtest=TestRunner -Dcucumber.filter.tags=$TYPE
+#mvn test -Dcucumber.filter.tags=$TYPE
+echo "API tests run completed..."
 
