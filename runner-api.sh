ee4b582964e193bdca2066ec3004c7785d88e37c
Automation test manager <ashkumark@gmail.com>
2023-10-31 14:36:44 +0000

    Updated dockerfile

--- runner-api.sh
+++ runner-api.sh
@@ -4,7 +4,6 @@
 
 echo "Run automated API tests (using runner script)..."
 #mvn -f pom.xml test -Dtest=TestRunner -Dcucumber.filter.tags=$TYPE
-docker ps
 mvn test -Dtest=TestRunner -Dcucumber.filter.tags=$TYPE
 #mvn test -Dcucumber.filter.tags=$TYPE
 echo "API tests run completed..."
